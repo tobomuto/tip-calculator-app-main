b5ea73312521a38249413aa7e6cae0f14c33295c
tobomuto <tombvr75@gmail.com>
2021-08-20 15:48:00 +0200

    something is wrong with percentage

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const tipSection = document.querySelector('#tip')
 const billSection = document.querySelector('#bill')
 const numberOfPeopleSection = document.querySelector('#number-of-people')
 
-const keys = document.getElementsByClassName('button');
+// const keys = document.getElementsByClassName('button');
+const checkboxes = document.querySelectorAll("div.button > input");
+const buttons = document.querySelectorAll("div.button > .checkbox");
 const resultTip = document.getElementById('tipResult');
 const resultTotal = document.getElementById('totalResult');
 const inputBill = document.querySelector('.bill-input');
@@ -15,39 +17,35 @@ let person = 0;
 let totalTip = 0;
 let totalPerPerson = 0;
 
-// console.log(inputPeople)
-
 //Event Listeners
+// buttons.addEventListener('click', setPercentage(this[index]))
+buttons.forEach(button => {
+    button.addEventListener('click', setPercentage)
+});
 inputBill.addEventListener('keyup', calculate);
 inputPeople.addEventListener('keyup', calculate);
 inputTip.addEventListener('keyup', calculate);
 
-const totalAmount = () => {
-    
-}
-
 function calculate() {
     person = inputPeople.value
     bill = inputBill.value
-
-    if (bill === 0 || person === 0) {
-        resultTotal.innerText = '$0.00';
-    } else if (bill > 0 && person > 0 ) {
-        resultTotal.innerText = ( bill / person ).toFixed(2);
-    }
-}
-
-const setPercentage = () => {
-
-}
-
-function calculateBill() {
-    if (bill === 0 || person === 0) {
-        console.log(totalPerPerson);
-        return (resultTotal.innerText = '$0.00');
+    if (!tipPercentage) {
+        if (bill === 0 || person === 0) {
+            resultTotal.innerText = '$0.00';
+        } else if (bill > 0 && person > 0 ) {
+            resultTotal.innerText = ( bill / person ).toFixed(2);
+        }
     } else {
-        totalPerPerson = bill / person;
-        console.log(totalPerPerson);
-        return (resultTotal.innerText = totalPerPerson);
+        totalTip = (( bill * tipPercentage ) / 100 ) / person;
+        return (resultTip.innerText = totalTip);
     }
+}
+
+function setPercentage() {
+    for (let i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].checked) {
+            tipPercentage = checkboxes[i].id;
+        }
+    }
+    calculate();
 }
